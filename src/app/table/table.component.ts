684903291a485b944e5fb489f4be7ec22dad4577
hussein406 <hussein406@hotmail.com>
2023-05-18 12:10:15 -0300

    design big change

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -3,18 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.css']
+  styleUrls: ['./table.component.css'],
 })
 export class TableComponent implements OnInit {
   items = [
-    { column1: 'PINCARG0_S4A_CSV_DELETION_AZ'},
-    { column1: 'PINCARG0_S4A_CSV_DELETION_AZ'},
-    { column1: 'PINCARG0_S4A_CSV_DELETION_AZ'},
-    { column1: 'PINCARG0_S4A_CSV_DELETION_AZ'},
+    { column1: 'PINCARG0_S4A_CSV_DELETION_AZ' },
+    { column1: 'PINCARG0_S4A_CSV_DELETION_AZ' },
+    { column1: 'PINCARG0_S4A_CSV_DELETION_AZ' },
+    { column1: 'PINCARG0_S4A_CSV_DELETION_AZ' },
   ];
 
   ngOnInit(): void {
-
-    console.log(this.items)
+    console.log(this.items);
   }
 }
